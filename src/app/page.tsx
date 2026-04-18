e03b2363353d616cf36a0dc8571a9f5b94aabf51
swordev7000 <swordev7000@gmail.com>
2026-04-18 00:04:35 -0400

    Adds background color to main section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { useTranslations } from "next-intl";
 export default function Home() {
   const t = useTranslations("HomePage");
   return (
-    <main>
+    <main className="bg-yellow-300">
       <section>{t("title")}</section>
     </main>
   );
